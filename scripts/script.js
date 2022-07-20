24352540d83d3e4bb5c9e0dfaef72eccbc5be60f
domster4444 <kshitiz.shah@deerwalk.edu.np>
2022-07-20 16:06:43 -0500

    feat(ui): implement openNav on cardhover

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
 const aside = document.getElementsByClassName('sidedrawer');
 console.log(aside[0]);
+
+const collegeCards = document.getElementsByClassName('college_cards');
+
+function openNav(eventParams) {
+  for (let i = 0; i < collegeCards.length; i++) {
+    collegeCards[i].addEventListener(eventParams, function () {
+      aside[0].classList.toggle('sidedrawer--inactive');
+      aside[0].classList.toggle('sidedrawer--active');
+    });
+  }
+}
+
+openNav('mouseover');
+openNav('mouseout');
+
+// Language: javascript
